8f038a6d494452959e6b1cc2a9a0db515ac9e4af
rustam <rustam@kamberov.me>
2017-12-02 10:43:36 +0100

    implemented random function and some of the object properties

--- scripts.js
+++ scripts.js
@@ -1,8 +1,49 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var simon = {
+        deviceStatus: 'off',
         gameStatus: 'off',
-        strictModeStatus: 'off'
+        strictModeStatus: 'off',
+        series: []
     };
 
-    console.log(simon.gameStatus);
+
+    var i = 0;
+    $('.quarter1').click(function () {
+        i++;
+        //console.log('clicked green');
+        //addNumberToSeries(i);
+        //console.log(simon.series);
+    });
+
+
+    function addNumberToSeries(number) {
+        simon.series.push(number);
+    }
+
+    function getRandom(min, max) {
+        var rMin = Math.ceil(min); //round to int greather
+        var rMax = Math.floor(max); //round to int lesse than specified number
+        var random = Math;
+        console.log(random);
+        return random;
+    }
+
+    /** Switch On the game */
+    $('.switch input').click(function () {
+
+        if ($('.switch input').is(':checked')) {
+            $('#start-restart-btn').removeClass('noselect');
+            simon.gameStatus = 'on';
+        } else {
+            $('#start-restart-btn').addClass('noselect');
+            simon.gameStatus = 'off';
+        }
+        console.log(simon.gameStatus);
+
+    });
+
+    $('#start-restart-btn').click(function () {
+        console.log('clicked');
+    });
+
 });
