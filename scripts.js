8181dc90eef90295c454035e898bc9bdfba5ee99
rustam <rustam@kamberov.me>
2017-12-08 08:48:28 +0100

    logic implemented

--- scripts.js
+++ scripts.js
@@ -9,27 +9,31 @@ $(document).ready(function() {
         isAnswerCorrect: true,
         sessionInterval: null,
         answerTimeout: null,
+        waitingTimeout: null,
         isAnswerExpected: false,
         guessed: 0,
         randomNumber: 0
     };
 
     $('#start-restart-btn').click(function() {
+        $('#info-display').text('---');
         simon.series = [];
         simon.answers = [];
         simon.stepCounter = 0;
         simon.isAnswerCorrect = true;
+        $('.quarter').removeClass('noselect');
         simon.sessionInterval = setInterval(startGame, 500);
     });
 
     function startGame() {
+
         console.log("ITERATION: " + simon.stepCounter);
-        console.log('random number is', simon.randomNumber);
+        $('#info-display').text(simon.stepCounter + 1);
+        //console.log('random number is', simon.randomNumber);
 
         clearInterval(simon.sessionInterval);
         clearTimeout(simon.answerTimeout);
         clearAllIntervals();
-        $('.quarter').removeClass('noselect');
 
         if (simon.isAnswerCorrect) {
             simon.randomNumber = getRandom();
@@ -41,37 +45,85 @@ $(document).ready(function() {
             console.log('series#' + simon.stepCounter, simon.series);
         }
 
+        playSounds();
+
+        $('.quarter').removeClass('noselect');
+
         var clickCounter = 0;
         $('.quarter').off().click(function() {
+            playSound('sound' + $(this).data('number'));
+            highlightArea($(this).data('number'));
+            clickCounter++;
             simon.guessed = parseInt($(this).data('number'));
-            if (simon.guessed === simon.series[clickCounter]) {
-                console.log('this time correct');
-                console.log('clickCounter', clickCounter);
-                console.log('simon.series.length', simon.series.length);
-                if (clickCounter === simon.series.length - 1) {
-                    console.log('all numbers are repeated correclty');
+            if (simon.guessed === simon.series[clickCounter - 1]) {
+                //console.log('this time correct');
+                //console.log('clickCounter', clickCounter);
+                //console.log('simon.series.length', simon.series.length);
+                if (clickCounter === simon.series.length) {
+                    console.log('all numbers are repeated correctly');
                     $('.quarter').addClass('noselect');
                     simon.answerTimeout = setTimeout(function() {
                         simon.isAnswerCorrect = true;
                         simon.stepCounter++;
-                        simon.sessionInterval = setInterval(startGame, 1000);
-                    }, 5000 + (simon.stepCounter * 1000));
+                        simon.sessionInterval = setInterval(startGame, 500);
+                    //}, 5000 + (simon.stepCounter * 1000));
+                    }, 2000);
                 }
             } else {
-                console.log('Wrong, you will given another chance');
+                playSound('fail');
+                console.log('Wrong, you will be given another chance');
+                $('.quarter').addClass('noselect');
                 simon.answerTimeout = setTimeout(function() {
                     simon.isAnswerCorrect = false;
                     simon.sessionInterval = setInterval(startGame, 1000);
                 }, 1000);
             }
-            clickCounter++;
+        //console.log('balance', clickCounter, simon.series.length);
         });
 
+        setTimeout(function() {
+            if (clickCounter === 0 || clickCounter < simon.series.length) {
+                playSound('fail');
+                simon.waitingTimeout = setTimeout(function() {
+                    clearTimeout(simon.answerTimeout);
+                    clearAllIntervals();
+                    console.log('You were given', (5000 + (simon.stepCounter * 1000)) / 1000 + ' seconds');
+                    simon.isAnswerCorrect = false;
+                    simon.sessionInterval = setInterval(startGame, 1000);
+                }, 1);
+            }
+        }, 5000 + (simon.stepCounter * 1000));
+    }
+
+    function playSounds() {
+        for (var i = 0; i < simon.series.length; i++) {
+            (function(index) {
+                setTimeout(function() {
+                    playSound('sound' + simon.series[index]);
+                    highlightArea(simon.series[index]);
+
+                //console.log(index, simon.series[index]);
+                }, i * 1000);
+            })(i);
+        }
+    }
+
+    function playSound(soundId) {
+        var sound = document.getElementById(soundId);
+        sound.play();
+    }
+
+    function highlightArea(id) {
+        $('#' + id).addClass('quarter' + id + '-light');
+        setTimeout(function() {
+            $('#' + id).removeClass('quarter' + id + '-light');
+        }, 300);
     }
 
     function clearAllIntervals() {
         for (var i = 1; i < 99999; i++) {
             window.clearInterval(i);
+            window.clearTimeout(i);
         }
     }
 
@@ -114,6 +166,7 @@ $(document).ready(function() {
         if ($('.switch input').is(':checked')) {
             $('#start-restart-btn').removeClass('noselect');
             $('#strict-btn').removeClass('noselect');
+            $('#info-display').text('---');
             //$('.quarter').removeClass('noselect');
             simon.deviceStatus = 'on';
         } else {
@@ -124,6 +177,7 @@ $(document).ready(function() {
             clearInterval(simon.sessionInterval);
             clearTimeout(simon.answerTimeout);
             clearAllIntervals();
+            $('#info-display').text('');
         }
 
     });
